f38a3ba50f91464e3e020daf6b16f501fc4f781f
Shawn Meier <shawn.meier@colorado.edu>
2020-09-23 22:04:12 -0600

    working hierarchy

--- soot_hopper/src/main/scala/edu/colorado/plv/bounder/solver/ClassHierarchy.scala
+++ soot_hopper/src/main/scala/edu/colorado/plv/bounder/solver/ClassHierarchy.scala
@@ -11,29 +11,31 @@ case class SuperclassOf(clazz:String) extends TypeConstraint
  * @param ctx z3 context to add class hierarchy assertions
  * @param types mapping from super types to sub types
  */
-class ClassHierarchy(ctx: Context, solver: Solver, types : List[(String,String)]) {
+class ClassHierarchy(ctx: Context, solver: Solver, types : Map[String,Set[String]]) {
 
-  val typeToInt: Map[String, Int] = types.flatMap(a => List(a._1, a._2)).toSet.zipWithIndex.toMap
+  val typeToInt: Map[String, Int] = types.keySet.zipWithIndex.toMap
   val intToType: Map[Int, String] = typeToInt.map(a => (a._2, a._1))
 
-  val tsort: Sort = ctx.mkFiniteDomainSort("Types", typeToInt.size)
 
+  val tsort: Sort = ctx.mkFiniteDomainSort("Types", typeToInt.size)
 
   private def finiteDomVal(t : String):Expr = {
     ctx.mkNumeral(typeToInt(t), tsort)
   }
+  val subtypeFun: FuncDecl = ctx.mkFuncDecl("subtype", Array(tsort, tsort), ctx.mkBoolSort())
   def mkHirearchyConstraints() {
     // Direct subclass function
-    val subtypeFun: FuncDecl = ctx.mkFuncDecl("directsubtype", Array(tsort, tsort), ctx.mkBoolSort())
    //   ctx.mkFreshFuncDecl("directsubtype", Array(tsort, tsort), ctx.mkBoolSort)
 
     val arg1 = ctx.mkBound(0, tsort)
     val arg2 = ctx.mkBound(1, tsort)
     val subclassConstraint =
-      types.foldLeft(ctx.mkFalse: Expr)({ case (acc, (supertype, subtype)) =>
-        val conj = ctx.mkAnd(ctx.mkEq(arg1, finiteDomVal(supertype)),
-          ctx.mkEq(arg2, finiteDomVal(subtype)))
-        ctx.mkITE(conj, ctx.mkTrue, acc)
+      types.foldLeft(ctx.mkFalse: Expr)({ case (acc, (supertype, subtypes)) =>
+        subtypes.foldLeft(acc) { case (acc, subtype) =>
+          val conj = ctx.mkAnd(ctx.mkEq(arg1, finiteDomVal(supertype)),
+            ctx.mkEq(arg2, finiteDomVal(subtype)))
+          ctx.mkITE(conj, ctx.mkTrue, acc)
+        }
       })
 
     val subtype_forall = ctx.mkForall(Array(tsort, tsort) /*sorts*/ ,
@@ -41,33 +43,18 @@ class ClassHierarchy(ctx: Context, solver: Solver, types : List[(String,String)]
       ctx.mkEq(subtypeFun.apply(arg1, arg2), subclassConstraint) /*body*/ , 1 /*weight*/ ,
       Array() /*patterns*/ , null, null, null)
     solver.add(subtype_forall)
-
-    // Transitive subclass function
-    val tS:FuncDecl = ctx.mkFuncDecl("subtype", Array(tsort, tsort), ctx.mkBoolSort())
-
-    val ta1 = ctx.mkBound(0, tsort)
-    val ta2 = ctx.mkBound(1, tsort)
-    val ta3 = ctx.mkBound(2, tsort)
-    val exists = ctx.mkExists(Array(tsort), Array(ctx.mkSymbol("ta3")),
-      ctx.mkAnd(ctx.mkEq(subtypeFun.apply(ta1, ta3), ctx.mkTrue),
-        ctx.mkEq(tS.apply(ta3, ta2), ctx.mkTrue)), 1, Array(), null, null, null )
-    val allT = ctx.mkForall(Array(tsort, tsort), Array(ctx.mkSymbol("ta1"), ctx.mkSymbol("ta2")),
-      ctx.mkEq(tS.apply(ta1, ta2), ctx.mkOr(ctx.mkEq(subtypeFun.apply(ta1, ta2), ctx.mkTrue), exists))/*body*/,
-      1, Array(), null, null, null)
-    solver.add(allT)
-
-    // It doesn't seem I can connect directsubtype like this
-    //ctx.mkForall(Array(tsort, tsort, tsort)/*sorts*/,
-    //  Array(ctx.mkSymbol("ta1"), ctx.mkSymbol("ta2"), ctx.mkSymbol("ta3"))/*symbols*/,
-    //  ctx.mkImplies(
-    //    ctx.mkAnd(ctx.mkEq(tS.apply(ta1,ta2), ctx.mkTrue()),ctx.mkEq(tS.apply(ta2,ta3),
-    //      ctx.mkTrue())),ctx.mkEq(tS.apply(ta1, ta3), ctx.mkTrue)) /*body*/
-    //  , 1, Array(), null, null, null)
-
   }
   mkHirearchyConstraints()
   solver.push()
 
 
-  def addTypeConstraint(vname: String, typeConstraint: TypeConstraint) = ???
+  def addTypeConstraint(vname: String, typeConstraint: TypeConstraint) = {
+    val const = ctx.mkConst(vname, tsort)
+    typeConstraint match {
+      case SubclassOf(c) =>
+        solver.add(ctx.mkEq(subtypeFun.apply(finiteDomVal(c), const), ctx.mkTrue))
+      case SuperclassOf(c) =>
+        solver.add(ctx.mkEq(subtypeFun.apply(const, finiteDomVal(c)), ctx.mkTrue))
+    }
+  }
 }
--- soot_hopper/src/test/scala/edu/colorado/plv/bounder/solver/ClassHierarchyTest.scala
+++ soot_hopper/src/test/scala/edu/colorado/plv/bounder/solver/ClassHierarchyTest.scala
@@ -1,17 +1,32 @@
 package edu.colorado.plv.bounder.solver
 
-import com.microsoft.z3.Context
+import com.microsoft.z3.{Context, Status}
 
 class ClassHierarchyTest extends org.scalatest.FunSuite {
   test("Subtype"){
     val ctx = new Context
-    val hierarchy = List(("Object", "String"), ("Object" , "Foo"), ("Foo", "Bar"))
+//    val hierarchy = List(("Object", "String"), ("Object" , "Foo"), ("Foo", "Bar"))
+    val hierarchy : Map[String, Set[String]] =
+      Map("Object" -> Set("String", "Foo", "Bar", "Object"),
+        "String" -> Set("String"), "Foo" -> Set("Bar", "Foo"), "Bar" -> Set("Bar"))
     val solver = ctx.mkSolver()
     val ch = new ClassHierarchy(ctx, solver, hierarchy)
-    println(s"check ${solver.check()}")
-    println("model")
-    println(solver.getModel())
-    println(s"assertions : ${solver.getAssertions().mkString(",")}")
+    assert(solver.check() == Status.SATISFIABLE)
+    ch.addTypeConstraint("a", SuperclassOf("Bar"))
+    ch.addTypeConstraint("a", SubclassOf("Foo"))
+//    println(solver.getAssertions.mkString("\n"))
+    assert(solver.check() == Status.SATISFIABLE)
+
+//    println(ch.typeToInt)
+    ch.addTypeConstraint("b", SuperclassOf("String"))
+    ch.addTypeConstraint("b", SubclassOf("String"))
+
+    assert(solver.check() == Status.SATISFIABLE)
+//    println(solver.getModel())
+
+    ch.addTypeConstraint("b", SuperclassOf("String"))
+    ch.addTypeConstraint("b", SubclassOf("Foo"))
+    assert(solver.check() == Status.UNSATISFIABLE)
     ctx.close()
   }
 
