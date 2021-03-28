84a1317fb88d774649e4ac485b1fe69a7e84ad8f
Shawn Meier <shawn.meier@colorado.edu>
2021-03-28 17:56:57 -0600

    run scripts

--- soot_hopper/setupJobDb.sh
+++ soot_hopper/setupJobDb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #docker pull postgres
-docker run -name bounder-db -e POSTGRES_PASSWORD=$(cat ~/.ssh/postgres-password) -d postgres
+docker run --name bounder-db -e POSTGRES_PASSWORD=$(cat ~/.ssh/postgres-password) -e POSTGRES_USER="bounder" -d postgres
---
+++ soot_hopper/tunnelPG.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -N -L 5433:localhost:5433 ubuntu@198.59.7.90
