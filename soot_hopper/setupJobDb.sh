e9f44147074b820dce51c7f7363f4f0dac8d133d
Shawn <you@example.com>
2021-03-29 00:43:58 +0000

    setup script fix pgpass

--- soot_hopper/setupJobDb.sh
+++ soot_hopper/setupJobDb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #docker pull postgres
-docker run --name bounder-db -e POSTGRES_PASSWORD=$(cat ~/.ssh/postgres-password) -e POSTGRES_USER="bounder" -d postgres
+docker run --name bounder-db -e POSTGRES_PASSWORD=$(cat ~/.pgpass) -e POSTGRES_USER="bounder" -d postgres
