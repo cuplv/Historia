56c7b7812a0f41b4f822f1dd764ecf313bc56ee5
Shawn Meier <shawn.meier@colorado.edu>
2022-03-15 13:31:52 -0600

    make run script start worker automatically and link pgpass

--- soot_hopper/runDockerLinux.sh
+++ soot_hopper/runDockerLinux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+#BOUNDER_JAR="not this one!!!!"
 if [ $# -eq 0 ]
 	then
 		#CMD=$(cat ~/dcmd)
-		docker run --rm --net=host -it --shm-size=1024m cuplv/bounder bash
-		#-c "java -jar $BOUNDER_JAR --mode expLoop"
+		docker run --rm --net=host -it --shm-size=1024m -v ~/.pgpass:/root/.pgpass -e BOUNDER_JAR='/home/bounder/target/scala-2.13/soot_hopper-assembly-0.1.jar' cuplv/bounder bash -c 'java -jar $BOUNDER_JAR --mode expLoop'
 	else
 		DATADIR=$(realpath $1)
 				echo "Data directory: $DATADIR"
