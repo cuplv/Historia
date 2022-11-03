3d53222a00cbb92fefcc2e61979381a0162addc7
Shawn Meier <shawn.meier@colorado.edu>
2022-11-03 12:39:12 -0600

    wok on enumerative synthesis

--- soot_hopper/src/test/scala/edu/colorado/plv/bounder/synthesis/Z3ModelGeneratorTest.scala
+++ soot_hopper/src/test/scala/edu/colorado/plv/bounder/synthesis/Z3ModelGeneratorTest.scala
@@ -9,6 +9,7 @@ import edu.colorado.plv.bounder.lifestate.{FragmentGetActivityNullSpec, LifeStat
 import edu.colorado.plv.bounder.solver.ClassHierarchyConstraints
 import edu.colorado.plv.bounder.symbolicexecutor.{QueryFinished, SparkCallGraph, SymbolicExecutorConfig, TransferFunctions}
 import edu.colorado.plv.bounder.symbolicexecutor.state.{AbstractTrace, CallStackFrame, CallinReturnNonNull, DBOutputMode, IPathNode, MemoryOutputMode, NamedPureVar, OrdCount, OutputMode, PathNode, PrettyPrinting, PureExpr, PureVal, PureVar, Qry, StackVar, State, StateFormula, TopVal, Unknown}
+import edu.colorado.plv.bounder.synthesis.SynthTestUtil.{hierarchy, intToClass, targetIze, top, witTreeFromMsgList}
 import edu.colorado.plv.bounder.testutils.MkApk
 import edu.colorado.plv.bounder.testutils.MkApk.makeApkWithSources
 import org.scalatest.funsuite.AnyFunSuite
@@ -166,66 +167,6 @@ class Z3ModelGeneratorTest extends AnyFunSuite {
   val t_create = SpecSignatures.RxJava_create_exit
   val a_call = SpecSignatures.RxJava_call_entry.copy(lsVars = TopVal::a::Nil)
 
-  def targetIze(history:List[LSSingle]):List[LSSingle] = {
-    def vTargetIze(pureExpr:PureExpr):PureExpr = pureExpr match {
-      case NamedPureVar(name) => NamedPureVar(s"${name}_tgt")
-      case other => other
-    }
-    history.map {
-      case LifeState.CLInit(sig) => ???
-      case LifeState.FreshRef(v) => ???
-      case Once(mt, signatures, lsVars) => Once(mt, signatures, lsVars.map(vTargetIze))
-    }
-  }
-
-  val dummyMethod = TestIRMethodLoc("","",Nil)
-  val dummyLoc = CallbackMethodInvoke("", "", dummyMethod)
-  val top = State.topState
-
-  /**
-   * Create a witness tree with top states from a list of abstract messages
-   * @param history list of abstract messages
-   * @param ord
-   * @param mode
-   * @return
-   */
-  def witTreeFromMsgList(history : List[LSSingle])
-                        ( implicit ord: OrdCount, mode: OutputMode = MemoryOutputMode):Set[IPathNode] = history match{
-    case at@_::t =>
-      val qry = Qry(top.copy(sf=top.sf.copy(traceAbstraction = AbstractTrace(at))), dummyLoc, Unknown)
-      //Set(PathNode(qry, witTreeFromMsgList(t).toList, None)) //TODO: test out full enc
-      Set(PathNode(qry, Nil, None))
-    case Nil => Set.empty
-  }
-
-  class DummyOrd extends OrdCount{
-
-    override def delta(current: Qry): Int = current.loc match {
-      case CallbackMethodInvoke(_, _, _) => 1
-      case CallbackMethodReturn(_, _, _, _) => 1
-      case _ => 0
-    }
-
-    override def compare(x: IPathNode, y: IPathNode): Int = ???
-  }
-
-  val hierarchy: Map[String, Set[String]] =
-    Map("java.lang.Object" -> Set("String", "Foo", "Bar",
-      "com.example.createdestroy.MyFragment",
-      "rx.Single",
-      "com.example.createdestroy.-$$Lambda$MyFragment$hAOPQ7FFP1lMCJX7gGOvwmZq1uk",
-      "java.lang.Object"),
-      "String" -> Set("String"), "Foo" -> Set("Bar", "Foo"), "Bar" -> Set("Bar"),
-      "com.example.createdestroy.MyFragment" -> Set("com.example.createdestroy.MyFragment"),
-      "com.example.createdestroy.-$$Lambda$MyFragment$hAOPQ7FFP1lMCJX7gGOvwmZq1uk" ->
-        Set("com.example.createdestroy.-$$Lambda$MyFragment$hAOPQ7FFP1lMCJX7gGOvwmZq1uk"),
-      "rx.Single" -> Set("rx.Single"),
-      "foo" -> Set("foo"),
-      "bar" -> Set("bar"),
-      "foo2" -> Set("foo2")
-    )
-  private val classToInt: Map[String, Int] = hierarchy.keySet.zipWithIndex.toMap
-  val intToClass: Map[Int, String] = classToInt.map(k => (k._2, k._1))
   implicit val ord = new DummyOrd
   implicit val outputMode = MemoryOutputMode
 
