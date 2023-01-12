b4052329a08125aa5e1e8fb2024c193d2b85b42c
Shawn Meier <shawn.meier@colorado.edu>
2023-01-12 13:17:04 -0700

    minor notebook changes

--- soot_hopper/experimentsDb/createTablesPg.sh
+++ soot_hopper/experimentsDb/createTablesPg.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-psql -h localhost -U postgres postgres -p 3333 -f setupPg.sql
+#psql -h localhost -U postgres postgres -p 3333 -f setupPg.sql
+psql -h localhost -U postgres postgres -p 5433 -f setupPg.sql
--- soot_hopper/experimentsDb/setupJobDb.sh
+++ soot_hopper/experimentsDb/setupJobDb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#docker pull postgres
+docker pull postgres
 PASS=$(sed 's/.*://' < ~/.pgpass)
 docker run --name bounder-db -p 5433:5432 -e POSTGRES_PASSWORD=$PASS -d postgres 
