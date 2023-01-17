fe3828ab8e0b6bef30b5b0901cc26fced885ee88
Shawn Meier <shawn.meier@colorado.edu>
2023-01-16 19:42:03 -0700

    notebooks

--- soot_hopper/runWorkers.sh
+++ soot_hopper/runWorkers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "" > ./containers
-./runDockerLinux.sh >> ./containers &
-./runDockerLinux.sh >> ./containers &
-./runDockerLinux.sh >> ./containers &
-./runDockerLinux.sh >> ./containers &
-./runDockerLinux.sh >> ./containers &
-./runDockerLinux.sh >> ./containers &
+./runDockerLinux.sh >> ./containers & #1
+./runDockerLinux.sh >> ./containers & #2
+./runDockerLinux.sh >> ./containers & #3
+./runDockerLinux.sh >> ./containers & #4
+./runDockerLinux.sh >> ./containers & #5
+./runDockerLinux.sh >> ./containers & #6
 
