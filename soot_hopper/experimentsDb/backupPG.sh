c9646b17e063a9c7b64618e78e9abab296f5606b
Shawn Meier <shawn.meier@colorado.edu>
2023-01-18 13:11:03 -0700

    notes on pg backup

--- soot_hopper/experimentsDb/backupPG.sh
+++ soot_hopper/experimentsDb/backupPG.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-pg_dump -h localhost -U postgres postgres -p 3333 > ~/Desktop/55select/apr-8-2022.sql
+pg_dump -h localhost -U postgres postgres -p 5433 > ~/Desktop/ossExpNulls/jan-18-2023.sql
 # note: use psql dbname < infile to restore
+
+# note2: for version mismatch between pg server and client:
+#sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+#wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+#sudo apt-get update
+#sudo apt-get install -y postgresql-client-11
