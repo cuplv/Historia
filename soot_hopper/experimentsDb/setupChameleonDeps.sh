ec3a7c1d2800697ac18ab9867cfaca7aa21a34b0
Shawn Meier <shawn.meier@colorado.edu>
2023-02-22 18:47:33 -0700

    script to install command line utils for reserving cc instances

---
+++ soot_hopper/experimentsDb/setupChameleonDeps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# install dependencies to access chameleon cloud from the command line
+pip install python-openstackclient
+pip install git+https://github.com/chameleoncloud/python-blazarclient@chameleoncloud/xena
