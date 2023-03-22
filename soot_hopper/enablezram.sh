5ac1afe5880ac9b788298834911224c4b4ab3ba5
root <root@historia.novalocal>
2023-03-22 01:41:43 +0000

    zram fix

--- soot_hopper/enablezram.sh
+++ soot_hopper/enablezram.sh
@@ -2,6 +2,6 @@
 # run with sudo
 echo 0 > /sys/module/zswap/parameters/enabled
 modprobe zram
-echo 100G > /sys/block/zram0/disksize
+echo 600G > /sys/block/zram0/disksize
 mkswap --label zram0 /dev/zram0
-swapon -p 600 /dev/zram0
+swapon -p 100 /dev/zram0
