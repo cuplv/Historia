a3e93dea9e44c4001757a1382baa9e5d684b43f5
Chameleon Cloud User <cc@historia.novalocal>
2023-03-22 01:39:33 +0000

    up zram to 600

--- soot_hopper/enablezram.sh
+++ soot_hopper/enablezram.sh
@@ -4,4 +4,4 @@ echo 0 > /sys/module/zswap/parameters/enabled
 modprobe zram
 echo 100G > /sys/block/zram0/disksize
 mkswap --label zram0 /dev/zram0
-swapon -p 100 /dev/zram0
+swapon -p 600 /dev/zram0
