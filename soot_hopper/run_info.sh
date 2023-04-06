2cae2a090adb6eca42207fbf11fdcd3a915b867c
Shawn Meier <shawn.meier@colorado.edu>
2023-04-06 17:45:57 -0600

    big table generation with jupyter

--- soot_hopper/run_info.sh
+++ soot_hopper/run_info.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-time java -Xmx12G -jar $HOME/Documents/source/Bounder/soot_hopper/target/scala-2.13/soot_hopper-assembly-0.1.jar \
--m info\
+time java -Xmx150G -jar $HOME/Documents/source/Bounder/soot_hopper/target/scala-2.13/soot_hopper-assembly-0.1.jar \
+-m info \
 -c $PWD/config_spec.json \
 -b $HOME/Documents/data/historia_generalizability \
 -u $PWD/spec \
--o MEM --debug
+-o DB --debug
 
