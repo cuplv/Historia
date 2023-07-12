8421c8e073ce930008ac0b589e984cb81cea52f2
Shawn Meier <shawn.meier@gmail.com>
2023-07-12 13:16:18 -0600

    exp script outputs results

--- implementation/runExperiments.sh
+++ implementation/runExperiments.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sbt clean
 sbt -J-Xmx16G "testOnly edu.colorado.plv.bounder.symbolicexecutor.Experiments"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cat "${SCRIPT_DIR}/log/logging.log" |grep "Row "
