d761eece96c358c7c786e7516bbef06027d26d07
Christian Fontenot <fontenot.christian@outlook.com>
2024-03-13 10:36:09 -0600

    All compilable except null_ptr

---
+++ testApps/ChatGPT_Benchmarks/EXN_configuration_changes/app_bug/app/src/main/java/com/example/configchange/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.configchange;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.lang.reflect.Field;
+
+public class MainActivity extends Activity {
+    static void dumpFields(Object o, Class clazz){
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+                Object fieldValue = field.get(o);
+                Log.i("histInstrumentation", "field " + field.getName() + " " + System.identityHashCode(fieldValue));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+    private EditText usernameInput;
+    private EditText emailInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameInput = findViewById(R.id.usernameInput);
+        emailInput = findViewById(R.id.emailInput);
+        // Initialize other fields here
+
+        // No handling of savedInstanceState to restore state
+    }
+
+}
---
+++ testApps/ChatGPT_Benchmarks/EXN_inefficient_network/app_bug/app/src/main/java/com/example/inefficientnetwork/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.inefficientnetwork;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    static void dumpFields(Object o, Class clazz){
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+                Object fieldValue = field.get(o);
+                Log.i("histInstrumentation", "field " + field.getName() + " " + System.identityHashCode(fieldValue));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+    private ListView listView;
+    private ArrayList<String> titles = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles);
+        listView.setAdapter(adapter);
+
+        fetchArticles();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fetchArticles(); // Inefficiently fetches articles every time the activity resumes
+    }
+
+    private void fetchArticles() {
+        String url = "https://www.google.com/search?q=android";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+                            titles.clear();
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                titles.add(jsonObject.getString("title"));
+                            }
+                            adapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Handle error
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+
+}
